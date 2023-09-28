0d8d8c6f7dddec44376efc2b68478dde82895f84
Manuel <manueljuliocasanova@gmail.com>
2023-09-28 16:10:01 -0400

    g

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,18 +12,26 @@ const Card = ({ title, description, imageSrc, link }) => {
       paddingBottom={'20px'}
     >
       <Image src={imageSrc} borderRadius={'8px'} />
-      <Heading
-        marginLeft={mL}
-        color={'black'}
-        textAlign={'left'}
-        height={['17px','17px','20px','20px']}
-        fontSize={['12px','25px','30px','30px']}
-      >
-        {title}
-      </Heading>
-      <Text marginLeft={mL} color={'gray'} fontSize={['10px','11px','12px','12px']}>
-        {description}
-      </Text>
+      <VStack height={'110px'}>
+        <Heading
+          marginLeft={mL}
+          color={'black'}
+          textAlign={'left'}
+          height={['17px', '17px', '20px', '20px']}
+          fontSize={['12px', '25px', '30px', '30px']}
+          marginBottom={'6px'}
+        >
+          {title}
+        </Heading>
+        <Text
+          marginLeft={mL}
+          color={'gray'}
+          fontSize={['10px', '11px', '12px', '12px']}
+        >
+          {description}
+        </Text>
+      </VStack>
+
       <HStack marginLeft={mL} alignItems={'center'}>
         <span style={{ color: 'black' }}>GitHub Repo</span>
         <a href={link}>
